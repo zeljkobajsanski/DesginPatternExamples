cde5ed6d2f0aac8b1ed721210db32ea27367815e
Zeljko Bajsanski <zeljko.bajsanski@enlightit.rs>
2018-09-20 14:00:56 +0200

    Strategy pattern

---
+++ DesignPatternExamples/Behavioral/Strategy/DuckExample/Behaviors.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace DesignPatternExamples.Behavioral.Strategy.DuckExample
+{
+    public interface IFlyBehavior
+    {
+        void Fly();
+    }
+
+    public class FlyWithWingsBehavior : IFlyBehavior
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Fly with wings");
+        }
+    }
+
+    public class NoFlyBehavior : IFlyBehavior
+    {
+        public void Fly()
+        {
+            Console.WriteLine("No fly");
+        }
+    }
+
+    public interface IQuackBehavior
+    {
+        void Quack();
+    }
+
+    public class QuackBehavior : IQuackBehavior {
+        public void Quack()
+        {
+            Console.WriteLine("Quack");
+        }
+    }
+
+    public class SqueakBehavior : IQuackBehavior {
+        public void Quack()
+        {
+            Console.WriteLine("Squeak");
+        }
+    }
+
+    public class MuteQuackBehavor : IQuackBehavior
+    {
+        public void Quack()
+        {
+            
+        }
+    }
+}
---
+++ DesignPatternExamples/Behavioral/Strategy/DuckExample/Ducks.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace DesignPatternExamples.Behavioral.Strategy.DuckExample
+{
+    public abstract class Duck
+    {
+        protected IQuackBehavior QuackBehavior;
+        protected IFlyBehavior FlyBehavior;
+        public virtual void Display() { }
+        public virtual void Swim() { }
+
+        public void Fly()
+        {
+            this.FlyBehavior.Fly();
+        }
+
+        public void Quack()
+        {
+            this.QuackBehavior.Quack();
+        }
+    }
+
+    public class MallardDuck : Duck
+    {
+        public MallardDuck()
+        {
+            FlyBehavior = new NoFlyBehavior();
+            QuackBehavior = new QuackBehavior();
+        }
+    }
+
+    public class RedheadDuck : Duck
+    {
+        public RedheadDuck()
+        {
+            FlyBehavior = new FlyWithWingsBehavior();
+            QuackBehavior = new QuackBehavior();
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("My head is red");
+        }
+    }
+
+    public class RubberDuck : Duck
+    {
+        public RubberDuck()
+        {
+            FlyBehavior = new NoFlyBehavior();
+            QuackBehavior = new SqueakBehavior();
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("I am a rubber duck");
+        }
+    }
+
+}
