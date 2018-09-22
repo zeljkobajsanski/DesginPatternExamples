a9b6ba81e69f3cb1b0956274835b18b35794e22b
Zeljko Bajsanski <zeljko.bajsanski@enlightit.rs>
2018-09-22 08:04:16 +0200

    Observer

---
+++ DesignPatternExamples/Behavioral/Observer/Observable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatternExamples.Behavioral.Observer
+{
+    public interface IObservable
+    {
+        void Subscribe(IObserver observer);
+        void Unsubscribe(IObserver observer);
+        void NotifyObservers();
+    }
+
+    public class Observable : IObservable
+    {
+        private readonly IList<IObserver> _observers = new List<IObserver>();
+
+        public void Subscribe(IObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Unsubscribe(IObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.Update("Foo", this);
+            }
+        }
+    }
+}
---
+++ DesignPatternExamples/Behavioral/Observer/Observer.cs
@@ -0,0 +1,7 @@
+﻿namespace DesignPatternExamples.Behavioral.Observer
+{
+    public interface IObserver
+    {
+        void Update(string value, IObservable subject);
+    }
+}
